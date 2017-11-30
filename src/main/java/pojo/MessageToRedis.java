be19d2f4cb14c6b6d384eb69950833e61f1bbf94
seanshi <496458172@qq.com>
2017-11-30 15:56:01 +0800

    添加blot

---
+++ src/main/java/pojo/MessageToRedis.java
@@ -0,0 +1,37 @@
+package pojo;
+
+import utils.DataType;
+
+public class MessageToRedis {
+
+    private String key;
+
+    private String values;
+
+    private DataType dataType;
+
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValues() {
+        return values;
+    }
+
+    public void setValues(String values) {
+        this.values = values;
+    }
+
+    public DataType getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(DataType dataType) {
+        this.dataType = dataType;
+    }
+}
---
+++ src/main/java/utils/DataType.java
@@ -0,0 +1,10 @@
+package utils;
+
+import java.io.Serializable;
+
+public enum DataType implements Serializable{
+
+    STRING,
+    SET
+
+}
